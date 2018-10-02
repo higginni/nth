80e0d302c5fb05036f61be58637270321e2cd023
higginni <higginni@tcd.ie>
2018-10-02 20:20:53 +0100

    added stuff

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            <code>nth consulting</code>
           </p>
           <a
             className="App-link"
-            href="https://reactjs.org"
+            href="mailto:higginni@tcd.ie"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Contact Us
           </a>
         </header>
       </div>
