346ce550155f4eec84d4af42beadb32e7a217d59
higginni <higginni@tcd.ie>
2018-10-25 23:23:46 +0100

    added new hero

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
-import React, { Component } from "react";
-import "./App.css";
+import React, { Component } from 'react'
+import Particles from 'react-particles-js'
+
+import './App.css'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Particles
+          params={{
+            particles: {
+              line_linked: {
+                shadow: {
+                  enable: true,
+                  color: '#3CA9D1',
+                  blur: 10
+                }
+              }
+            }
+          }}
+          style={{
+            width: '100%'
+          }}
+        />
         <header className="App-header">
           <p>
             <code>nth degree consulting</code>
           </p>
+          <p>Small and medium size digital consulting</p>
           <a
             className="App-link"
             href="mailto:higginni@tcd.ie"
@@ -19,8 +38,8 @@ class App extends Component {
           </a>
         </header>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
